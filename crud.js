754b64a57204b688857a42a84ac367ff967fc8b7
Chetan Kumar <chetankumar8203@gmail.com>
2023-01-15 12:20:28 +0530

    crud

---
+++ crud.js
@@ -0,0 +1,12 @@
+//What are different http methods do you have ?
+
+//C - Creat --> //POST
+//R - Read --> //GET
+
+//U - UPDATE --> //PUT
+//D - DELETE --> //DELETE
+
+//OPTIONS
+
+
+//Get and POST
